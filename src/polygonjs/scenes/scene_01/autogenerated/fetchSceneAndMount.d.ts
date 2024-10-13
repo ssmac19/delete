b3c99ae37c9223a0ca75879c583aaba34b13198b
ssmac19 <42553254+ssmac19@users.noreply.github.com>
2024-10-13 17:16:03 -0400

    Delete src directory

--- src/polygonjs/scenes/scene_01/autogenerated/fetchSceneAndMount.d.ts
+++
@@ -1,7 +0,0 @@
-import { LoadSceneOptions, LoadedData } from "./loadScene";
-
-type FetchSceneAndMount_scene_01 = (
-  options?: LoadSceneOptions
-) => Promise<LoadedData>;
-
-export const fetchSceneAndMount_scene_01: FetchSceneAndMount_scene_01;
